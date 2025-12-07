30ddb224ac4153d1bb6272e90f1d1192ce5f0e2d
Dmode <dmode90@gmail.com>
2025-12-07 16:50:05 +0200

    set en lang to default fix

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,7 +3,7 @@ import { cookies } from 'next/headers';
  
 export default getRequestConfig(async () => {
   const store = await cookies();
-  const locale = store.get('locale')?.value || 'uk';
+  const locale = store.get('locale')?.value || 'en';
  
   return {
     locale,
